92a40605c77cdad1d8bf36751437b15b1c325811
Jeffreyt83 <jeffreyt01@hotmail.com>
2022-11-07 21:42:55 +0000

    Optional Challenge

---
+++ advlist/animal-list.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python3
+
+"""
+J.Thomas Learning Python
+
+List of Animals
+"""
+
+def main():
+
+    #create a list of animals
+    animals1 = ["Fox", "Dog", "Yak"]
+    
+    #display list of animals
+    print(animals1)
+
+    #create a new list of animals
+    animals2 = ["Cow", "Tiger", "Lion"]
+
+    #extend list of animals
+    animals1.extend(animals2)
+
+    #display animals1 list, which now contains animals from animals2 list
+    print(animals1)
+
+    #create list 3
+    animals3 = ["Zebra", "ELephant"]
+
+    #print the second item in animals3 list
+    print(animals3[1])
+
+    #extend list of animals
+    animals1.extend(animals3)
+
+    #display the new long list of animals
+    print(animals1)
+
+main()
