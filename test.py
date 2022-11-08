cc36515832ad850d0e4ea1068b42757f8bde06b9
Jeffreyt83 <jeffreyt01@hotmail.com>
2022-11-08 17:38:04 +0000

    renaming and moving files

---
+++ test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+def main():
+
+    #ask user for value of X
+    x = input("Provide value of X: ")
+
+    #print value of X
+    print(x)
+
+if __name__ == "__main__":
+        main()
